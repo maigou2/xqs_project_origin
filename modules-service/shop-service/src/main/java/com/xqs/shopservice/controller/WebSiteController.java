127eaff24f3b76d46fd6b3661b0df5922a18934d
Maigou <1317873153@qq.com>
2024-05-24 16:46:54 +0800

    完善项目结构

--- modules-service/shop-service/src/main/java/com/xqs/shopservice/controller/WebSiteController.java
+++ modules-service/shop-service/src/main/java/com/xqs/shopservice/controller/WebSiteController.java
@@ -1,5 +1,6 @@
 package com.xqs.shopservice.controller;
 
+import com.xqs.commoncore.base.response.BaseResponseDTO;
 import com.xqs.shopservice.service.WebSiteService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,10 @@ public class WebSiteController {
     @Resource
     private WebSiteService webSiteService;
 
-
+    @RequestMapping("/addCommodity")
+    public BaseResponseDTO addCommodity() {
+        return BaseResponseDTO.success();
+    }
 
 
 }
